ad063c72ec66cc76aaa7b797be32d0d7c720c513
Nathaniel Cook <nvcook42@gmail.com>
2013-10-03 22:30:18 -0600

    move stuff around

--- src/morgoth/window.finalize.js
+++
@@ -1,8 +0,0 @@
-function (key, value) {
-    var len = value.count * 10 + value.P.length;
-    for (var i = 0; i < value.P.length; i++) {
-        value.P[i] /= len;
-    }
-    delete value['discrete'];
-    return value;
-}
--- src/morgoth/window.reduce.js
+++
@@ -1,9 +0,0 @@
-function (key, values) {
-    var a = values[0];
-    for ( var i = 1; i < values.length; i++) {
-        var b = values[i];
-        a.P[b.discrete] += 10;
-        a.count += b.count;
-    }
-    return a;
-}
