9da590f50ca08af57c995c3f16f9b4726e88549b
Nathaniel Cook <nvcook42@gmail.com>
2014-06-11 20:19:05 -0600

    update logos

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -131,7 +131,7 @@ html_theme_path = ['theme']
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-html_logo = '../logo_200.png'
+html_logo = '../../logo_200.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
