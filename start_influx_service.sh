76409fc78cde8336fe8d9e611457b9946ae4bef0
Nathaniel Cook <nvcook42@gmail.com>
2014-07-18 20:35:30 -0600

    trying upstream since mine fork isnt compiling

--- start_influx_service.sh
+++ start_influx_service.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-git clone https://github.com/nvcook42/influxdb.git
+#git clone https://github.com/nvcook42/influxdb.git
+git clone https://github.com/influxdb/influxdb.git
 
 cd influxdb
 ./configure
-make
+make build
+ls -l .
 ./daemon
 
