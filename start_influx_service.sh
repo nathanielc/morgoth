edd60ae356e57ce409ee8d6a7a26c257082ef6aa
Nathaniel Cook <nathanielc@qualtrics.com>
2014-07-25 10:25:20 -0600

    use my influxdb fork

--- start_influx_service.sh
+++ start_influx_service.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-#git clone https://github.com/nvcook42/influxdb.git
-git clone https://github.com/influxdb/influxdb.git
+git clone https://github.com/nvcook42/influxdb.git
+#git clone https://github.com/influxdb/influxdb.git
 
 cd influxdb
 ./configure
