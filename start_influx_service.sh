6adf3b21328f12d113364c98f0940d0ad1e79f0b
Nathaniel Cook <nvcook42@gmail.com>
2014-07-29 22:28:56 -0600

    make start influx smarter

--- start_influx_service.sh
+++ start_influx_service.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
+set -e
 
 mkdir -p $GOPATH/src/github.com/influxdb
 cd $GOPATH/src/github.com/influxdb
-git clone https://github.com/nvcook42/influxdb.git
-cd influxdb
+if [ -d influxdb ]
+then
+    cd influxdb
+    git pull
+else
+    git clone https://github.com/nvcook42/influxdb.git
+    cd influxdb
+fi
 
 ./configure
 make build
-screen ./influxdb
+screen -d -m ./influxdb || cat influxdb.log
 
