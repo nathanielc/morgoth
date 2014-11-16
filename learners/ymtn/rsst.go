dded3be9984cc5170f0f0879e6281ed3dd9cf476
Nathaniel Cook <nvcook42@gmail.com>
2014-11-15 22:47:19 -0700

    imporving the rsst function

--- learners/ymtn/rsst.go
+++ learners/ymtn/rsst.go
@@ -6,37 +6,92 @@ import (
 	"github.com/nvcook42/linalg/blas"
 	"github.com/nvcook42/linalg/lapack"
 	"github.com/nvcook42/matrix"
-	//"math"
+	"math"
+)
+
+const (
+	small = 1e-9
 )
 
 func RSST(x []float64, w, n int) []float64 {
 
 	T := len(x)
-	stop := w * n
+	m := n
+	g := 0
 
-	i := 0
-	size := T - stop * 2
-	changeScores := make([]float64, size, size)
-	for t := stop; t < T - stop; t++ {
-		m := n
-		g := 0
+	changeScores := make([]float64, T)
+	for t := w + n; t < T-w-m-g; t++ {
 
-		u := calcU(x, t, w, n)
-		beta, lambda := calcB(x, t, g, w, m)
+		past := calcPast(x, t, w, n)
+		future, eigenValues := calcFuture(x, t, g, w, m)
 
-		fmt.Println("U: ", u)
-		fmt.Println("B: ", beta)
-		fmt.Println("L: ", lambda)
+		fmt.Println("Past: ", past)
+		fmt.Println("Future: ", future)
+		fmt.Println("Eigen Values: ", eigenValues)
 
-		changeScores[i] = calcChangeScore(u, beta, lambda)
-
-		i++
+		//Only calc changescores if our eigenvalues are not small
+		if eigenValues.GetAt(0, 0) > small {
+			changeScores[t] = calcChangeScore(past, future, eigenValues)
+		}
 	}
+
+	//Weight each score by it's past and future
 	fmt.Println("CS: ", changeScores)
+	width := w / 2
+	start := w + n
+	if start < width {
+		start = width
+	}
+	for t := start; t < T-width-1; t++ {
+		pastMean, pastVar := calcMeanVar(changeScores[t-width : t])
+		futureMean, futureVar := calcMeanVar(changeScores[t : t+width])
+		//fmt.Println(pastMean, pastVar, futureMean, futureVar)
+		changeScores[t] *= math.Abs(pastMean-futureMean) *
+			math.Abs(math.Sqrt(pastVar)-math.Sqrt(futureVar))
+	}
+
+	fmt.Println("CS adjusted: ", changeScores)
+	//Keep only local maxima
+	maxima := make([]float64, T)
+	max := -1.0
+	for i, v := range changeScores {
+		if (i == 0 || v > changeScores[i-1]) &&
+			(i == T-1 || v > changeScores[i+1]) {
+			maxima[i] = v
+			if v > max {
+				max = v
+			}
+		}
+	}
+
+	for i := range maxima {
+		maxima[i] /= max
+	}
+
+	fmt.Println("Normal Maxima: ", maxima)
 	return nil
 }
 
-func calcSHerkel(x []float64, t, w, n int) *matrix.FloatMatrix {
+func calcMeanVar(x []float64) (float64, float64) {
+	l := float64(len(x))
+	sum := 0.0
+	for _, v := range x {
+		sum += v
+	}
+	mean := sum / l
+	varSum := 0.0
+	for _, v := range x {
+		diff := v - mean
+		varSum += diff * diff
+	}
+
+	variance := varSum / l
+
+	fmt.Println(l, sum, mean, variance, x)
+	return mean, variance
+}
+
+func calcPastHerkel(x []float64, t, w, n int) *matrix.FloatMatrix {
 
 	herkel := matrix.FloatZeros(w, n)
 	for wi := 0; wi < w; wi++ {
@@ -48,13 +103,28 @@ func calcSHerkel(x []float64, t, w, n int) *matrix.FloatMatrix {
 	return herkel
 }
 
-func calcl(sigma *matrix.FloatMatrix) int {
-	//l := sigma.Rows()
-	//return l
-	return 2
+//Find the number of eigenvectors to use based on
+// the corner of the accumlative sum of sigma
+func calcNumEigenValues(sigma *matrix.FloatMatrix) int {
+	l := sigma.Rows()
+	if sigma.GetAt(0, 0) < small || l == 1 {
+		return 1
+	}
+
+	sum := 0.0
+	cumsum := make([]float64, l)
+	for i := 0; i < l; i++ {
+		sum += sigma.GetAt(i, 0)
+		cumsum[i] = sum
+	}
+
+	i := 1
+	for ; cumsum[i]/sum < 0.96; i++ {
+	}
+	return i
 }
 
-func calcGHerkel(x []float64, t, g, w, m int) *matrix.FloatMatrix {
+func calcFutureHerkel(x []float64, t, g, w, m int) *matrix.FloatMatrix {
 	herkel := matrix.FloatZeros(w, m)
 	for wi := 0; wi < w; wi++ {
 		for mi := 0; mi < m; mi++ {
@@ -65,9 +135,8 @@ func calcGHerkel(x []float64, t, g, w, m int) *matrix.FloatMatrix {
 	return herkel
 }
 
-
-func calcU(x []float64, t, w, n int) *matrix.FloatMatrix {
-	herkel := calcSHerkel(x, t, w, n)
+func calcPast(x []float64, t, w, n int) *matrix.FloatMatrix {
+	herkel := calcPastHerkel(x, t, w, n)
 	size := w
 	if n < w {
 		size = n
@@ -75,7 +144,6 @@ func calcU(x []float64, t, w, n int) *matrix.FloatMatrix {
 	sigma := matrix.FloatZeros(size, 1)
 	u := matrix.FloatZeros(w, w)
 	vt := matrix.FloatZeros(n, n)
-
 
 	lapack.GesvdFloat(
 		herkel,
@@ -85,11 +153,9 @@ func calcU(x []float64, t, w, n int) *matrix.FloatMatrix {
 		&linalg.IOpt{"jobu", linalg.PJobAll},
 		&linalg.IOpt{"jobvt", linalg.PJobAll},
 	)
-	//fmt.Printf("U: %s\n", u)
-	//fmt.Printf("S: %s\n", sigma)
-	//fmt.Printf("Vt: %s\n", vt)
 
-	l := calcl(sigma)
+	l := calcNumEigenValues(sigma)
+	fmt.Println("lp: ", l)
 
 	sub := matrix.FloatZeros(w, l)
 	u.SubMatrix(sub, 0, 0, w, l)
@@ -97,8 +163,8 @@ func calcU(x []float64, t, w, n int) *matrix.FloatMatrix {
 	return sub
 }
 
-func calcB(x []float64, t, g, w, m int) (*matrix.FloatMatrix, *matrix.FloatMatrix) {
-	herkel := calcGHerkel(x, t, g, w, m)
+func calcFuture(x []float64, t, g, w, m int) (*matrix.FloatMatrix, *matrix.FloatMatrix) {
+	herkel := calcFutureHerkel(x, t, g, w, m)
 	size := w
 	if m < w {
 		size = m
@@ -116,46 +182,59 @@ func calcB(x []float64, t, g, w, m int) (*matrix.FloatMatrix, *matrix.FloatMatri
 		&linalg.IOpt{"jobvt", linalg.PJobNo},
 	)
 
-	l := calcl(sigma)
+	l := calcNumEigenValues(sigma)
+	fmt.Println("lf: ", l)
 
 	sub := matrix.FloatZeros(w, l)
 	u.SubMatrix(sub, 0, 0, w, l)
 
-	//Lambdas are the squares of sigmas
-	for i := 0; i < size; i++ {
+	eigenValues := matrix.FloatZeros(l, 1)
+
+	//Eigen values are the squares of sigmas
+	for i := 0; i < l; i++ {
 		v := sigma.GetAt(i, 0)
-		sigma.SetAt(i, 0, v*v)
+		eigenValues.SetAt(i, 0, v*v)
 	}
 
-	return sub, sigma
+	return sub, eigenValues
 }
 
-func calcChangeScore(u, beta, lambda *matrix.FloatMatrix) float64 {
+func calcChangeScore(past, future, eigenValues *matrix.FloatMatrix) float64 {
 
-	w := u.Rows()
-	lf := beta.Cols()
+	w := past.Rows()
+	lf := future.Cols()
 	b := matrix.FloatZeros(w, 1)
 	v := matrix.FloatZeros(lf, 1)
-	lambdaSum := 0.0
+	eigenValuesSum := 0.0
 	csSum := 0.0
 	for i := 0; i < lf; i++ {
-		beta.SubMatrix(b, 0, i, w, 1)
-		fmt.Println("b: ", b)
+		future.SubMatrix(b, 0, i, w, 1)
 		blas.Gemv(
-			u,
+			past,
 			b,
 			v,
 			matrix.FScalar(1.0),
 			matrix.FScalar(0.0),
 			linalg.OptTrans,
 		)
-		norm := blas.Nrm2(v)
-		a := v.Scale(1.0/norm.Float())
+		fmt.Println("P: ", past)
+		fmt.Println("B: ", b)
+		fmt.Println("V: ", v)
+		norm := blas.Nrm2(v).Float()
+		var a *matrix.FloatMatrix
+		if norm != 0 {
+			a = v.Scale(1.0 / norm)
+		} else {
+			//This shouldn't happen, looking into why
+			a = v
+		}
+		fmt.Println("A: ", a)
 		cs := 1 - blas.Dotu(a, b).Float()
-		lambdaI := lambda.GetAt(i, 0)
-		csSum += cs * lambdaI
-		lambdaSum += lambdaI
+		eigenValue := eigenValues.GetAt(i, 0)
+		csSum += cs * eigenValue
+		eigenValuesSum += eigenValue
 	}
 
-	return csSum / lambdaSum
+	fmt.Println(csSum, eigenValuesSum)
+	return csSum / eigenValuesSum
 }
--- learners/ymtn/rsst_test.go
+++ learners/ymtn/rsst_test.go
@@ -4,21 +4,23 @@ import (
 	"github.com/nvcook42/morgoth/learners/ymtn"
 	"github.com/stretchr/testify/assert"
 	"math/rand"
-	//"math"
+	"math"
 	"testing"
 )
 
 func TestRSST(t *testing.T) {
 	assert := assert.New(t)
 
-	size := 100
+	size := 1000
 	rand.Seed(42)
 	x := make([]float64, size, size)
 	for i := range x {
-		//x[i] = math.Sin(float64(i))
-		x[i] = float64(i)
+		x[i] = math.Sin(float64(i))
+		if i % 2 == 0 {
+			x[i] = float64(i)
+		}
 	}
-	xs := ymtn.RSST(x, 5, 4)
+	xs := ymtn.RSST(x, 5, 5)
 	assert.NotNil(xs)
 
 }
